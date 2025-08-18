233948e497ca7eb469272896691f2cf3fa95c37a
Anushkaa-80 <anushkaa8090@gmail.com>
2025-08-18 15:49:23 +0530

    Add cookie-parser and cors dependencies, create utility classes for API error and response handling, and add async handler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandler = (requestHandler) => {
+  (req, re, next) => {
+    Promise.resolve(requestHandler(req, resizeBy, next)).catch((err) =>
+      next(err)
+    );
+  };
+};
+
+export { asyncHandler };
+
+//const asyncHandler = (fn) =>() =>{}
+// const asyncHandler = (func) =>()=>{}
+//const asyncHandler =(func)=> async()=>{}
+
+/*
+
+const asyncHandler = (fn) => async (req,res,next) => {
+    try{
+        await fn(req,res,next)
+    }
+    catch(error)
+    {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}*/
